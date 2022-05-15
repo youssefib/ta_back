b79da6b447cc82860c2482eb98ebaa704476f4ab
youssef ibaichou <youssef.ibaichou@gmail.com>
2022-05-15 01:31:20 +0100

    jwt config

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    function index(){
+        $users = User::all();
+
+        return $users;
+    }
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix("auth")->group(function(){
+    Route::post('login', [AuthController::class, 'login']);
+});
+
+
+Route::prefix("users")->middleware("auth:api")->group(function(){
+    Route::get('/', [UserController::class, 'index']);
 });
